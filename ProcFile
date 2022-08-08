48230ae6a438e7f91e2af626861e8b1eb3d309a3
rahulchatterjee12 <rc5292684@gmail.com>
2022-08-08 22:32:17 +0530

    Initial commit

--- ProcFile
+++ ProcFile
@@ -1,1 +1,1 @@
-web: gunicorn project.wsgi
+web: gunicorn CCMS.wsgi --log-file -
--- project/settings.py
+++ project/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure-0g=@m_o*t6(bz6$x$zqqtq^^di1xgow75)l@zo%gt1@!_dy0jl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
