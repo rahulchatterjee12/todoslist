a2d2bf332714a1df862c951649181161e0c742ad
rahulchatterjee12 <rc5292684@gmail.com>
2022-08-09 00:10:11 +0530

    Initial commit

--- project/settings.py
+++ project/settings.py
@@ -34,13 +34,13 @@ SECRET_KEY = 'django-insecure-0g=@m_o*t6(bz6$x$zqqtq^^di1xgow75)l@zo%gt1@!_dy0jl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','todoslistrc.herokuapp.com']
+# ALLOWED_HOSTS = ['127.0.0.1','todoslistrc.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'project',
     'home',
     'django.contrib.admin',
     'django.contrib.auth',
